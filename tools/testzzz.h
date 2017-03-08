32314f02d85d20f58043c81486363d53b8996c64
strongrobot <j2ee_weihai@sina.cn>
2017-03-08 16:10:50 +0800

    JNI/JNA

---
+++ tools/testzzz.h
@@ -0,0 +1,14 @@
+//
+// Created by zzz on 17-3-8.
+//
+
+#ifndef DRACO_TESTZZZ_H
+#define DRACO_TESTZZZ_H
+
+#endif //DRACO_TESTZZZ_H
+
+extern "C"{
+    int add(int a, int b);
+
+    int compress(char* inputFile,char* outputFile);
+}
